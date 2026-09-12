bf3e3a85c4ac9bd48c65115d4bf400b8b9cd513a
cookieLOADER <rigbycat38@gmail.com>
2026-09-11 20:43:13 -0400

    Add AutoMake.py for APK creation tutorial
    This script provides a tutorial for creating APKs, including links to necessary tools and a Discord channel for support.

---
+++ AutoMake.py
@@ -0,0 +1,26 @@
+import os 
+import webbrowser 
+import time
+
+#start
+print("|- MADE BY RIGBY --  JOIN DCM -- https://discord.gg/kTgSVTykkn -|")
+time.sleep(0.6)
+print("These are the tools you need to make the apks. Wait 3 seconds")
+time.sleep(3)
+os.system("cls")
+webbrowser.open("https://notepad-plus-plus.org/downloads")
+webbrowser.open("https://chromewebstore.google.com/detail/downq/clocmpojdjmikkaepgkmplgooejmnchb?pli=1")
+webbrowser.open("https://github.com/mkcs121/APK-Easy-Tool/releases")
+webbrowser.open("https://www.pythonanywhere.com")
+user = input("Hello please spell and enter the word --> run. So the full tutorial comes? --> ")
+if user == "run":
+    os.system("cls")
+    print("Alright lets get running. Please join are Discord, if you really need help on modding or help for this tutorial. We got a text file tutorial.")
+    print("https://discord.gg/HHVUQWw5rE")
+    time.sleep(6)
+    os.system("cls")
+
+#after start
+print("1. Make a pythonanywhere account. The tab should have been open in ur browser right now. Please make a new account with an email and use a username thats this exact long q7m2x9kpl4v8zzz123 One mistake will make this whole thing not work.")
+print("2. Download all the stuff needed added inside ur browser tabs!!")
+print("FINAL STEP: Join the discord for the text file tutorial which has all of the tutorial in text. And this was fully made by DCM Im saying the method. How did we figure it out? Well commands that find shit. Thats how. How long did you guys know this? Like 2 months we made videos you can find it on my tiktok. Why did you guys release the method? Well this is what happened. I was sitting doing nothing but dreaming. Then I get a bunch of calls and messages, on how I WAS LEAKING. But no I didnt. But they showed me a github link, then boom thats when I found out someone else figured the method. But they didnt know what they were talking about. And yea. Thats why we released, but like fuck yall are ass and ass we are better than you.")
